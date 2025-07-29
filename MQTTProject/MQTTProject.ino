3c0c96568f3f88765aac4a38011161bd84a20222
Ryan Osswald <raosswald@gmail.com>
2025-07-29 14:35:31 -0400

    Added Arduino project

---
+++ MQTTProject/MQTTProject.ino
@@ -0,0 +1,136 @@
+#include "TFT_eSPI.h"
+#include "rpcWiFi.h"
+#include <PubSubClient.h>
+
+// Declare Global Variables
+TFT_eSPI tft;
+const char* ssid = "SSIDHERE";
+const char* password = "PASSWORDHERE";
+
+// MQTT Variables
+const char *ID = "Wio-Terminal-Client";
+const char *TOPIC = "LEDControl";
+const char *subTopic = "Temperature";
+const char *server = "TestBedRpi";
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+void bgRefresh() {
+  tft.fillScreen(TFT_WHITE);
+  tft.setTextColor(TFT_RED);
+  tft.setTextSize(2);
+  tft.drawString("Red", 5, 0);
+  tft.setTextColor(TFT_GREEN);
+  tft.drawString("Green", 60, 0);
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("Blue", 150, 0);
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String mssg = "";
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+    mssg += (char)payload[i];
+  }
+  Serial.println(mssg);
+  bgRefresh();
+  tft.setTextColor(TFT_BLACK);
+  tft.drawString(mssg, 5, 50);
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ID)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(TOPIC, "WT Conn");
+      Serial.println("Published connection message successfully!");
+      // ... and resubscribe
+      client.subscribe(subTopic);
+      Serial.print("Subcribed to: ");
+      Serial.println(subTopic);
+    }
+    else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial);
+  delay(1000);
+
+  //Setup Wifi
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED ) {
+    delay(500);
+    Serial.println("Connecting to Wifi...");
+    WiFi.begin(ssid, password);
+  }
+  Serial.println("Connected to WiFi network");
+  Serial.print("IP Address: ");
+  Serial.println(ssid);
+
+  //MQTT Setup
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+  // SetupLCD Screen
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_WHITE);
+
+  tft.setTextColor(TFT_RED);
+  tft.setTextSize(2);
+  tft.drawString("Red", 5, 0);
+  tft.setTextColor(TFT_GREEN);
+  tft.drawString("Green", 60, 0);
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("Blue", 150, 0);
+
+  // Setup Buttons
+  pinMode(WIO_KEY_A, INPUT_PULLUP); //Rightmost button
+  pinMode(WIO_KEY_B, INPUT_PULLUP);
+  pinMode(WIO_KEY_C, INPUT_PULLUP); //Leftmost button
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(!client.connected()) {
+    reconnect();
+  }
+  if (digitalRead(WIO_KEY_A) == LOW) {
+    Serial.println("A Key pressed");
+    client.publish(TOPIC, "BLUE");
+  }
+  else if (digitalRead(WIO_KEY_B) == LOW) {
+    Serial.println("B Key pressed");
+    client.publish(TOPIC, "GREEN");
+  }
+  else if (digitalRead(WIO_KEY_C) == LOW) {
+    Serial.println("C Key pressed");
+    client.publish(TOPIC, "RED");
+  }
+  delay(200);
+
+  client.loop();
+}
